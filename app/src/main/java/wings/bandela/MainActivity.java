278a74b5f83138d43ae9d8332af77482657279fc
zzhengz <zarickzheng@gmail.com>
2015-07-16 13:08:49 -0500

    Gimbal beacon testing
    That's the code that works on Luke's phone but fails on Zheng's phone
    due to the Android version issue.

--- app/src/main/java/wings/bandela/MainActivity.java
+++ app/src/main/java/wings/bandela/MainActivity.java
@@ -1,77 +1,48 @@
 package wings.bandela;
 
-import com.gimbal.android.Gimbal;
-import com.gimbal.android.CommunicationManager;
-import com.gimbal.android.CommunicationListener;
-import com.gimbal.android.Communication;
-import com.gimbal.android.Push;
-import com.gimbal.android.PlaceManager;
-import com.gimbal.android.PlaceEventListener;
-import com.gimbal.android.Place;
-import com.gimbal.android.Visit;
-import com.gimbal.android.BeaconEventListener;
-import com.gimbal.android.BeaconManager;
-import com.gimbal.android.BeaconSighting;
-
-import com.gimbal.proximity.ProximityListener;
-import com.gimbal.proximity.ProximityOptions;
-
-/* potentially unwanted imports. These suggested imports were found under a section
-//stating that these imports should be included if using CommunicationListener and
-//CommunicationManager instances.
-
-import java.sql.Date;
-import java.util.Collection;
-import java.util.List;
-import android.util.Log;
-*/
-
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
 import android.support.v7.app.ActionBarActivity;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends Activity {
+import com.gimbal.android.CommunicationManager;
+import com.gimbal.android.Gimbal;
+import com.gimbal.android.PlaceEventListener;
+import com.gimbal.android.PlaceManager;
+import com.gimbal.android.Visit;
+
+
+public class MainActivity extends ActionBarActivity {
 
     private PlaceManager placeManager;
     private PlaceEventListener placeEventListener;
-    private CommunicationListener communicationListener;
-    private BeaconEventListener beaconSightingListener;
-    private BeaconManager beaconManager;
-    private static final String TAG = "MyActivity";
+    private ArrayAdapter<String> listAdapter;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*
-            TESTING start for Gimbal code.
-         */
-        Gimbal.setApiKey(this.getApplication(), "41238e20-69b3-48f7-b5b5-9648d9ba4dfb"); //"## PLACE YOUR API KEY HERE ##"
 
-        //Proximity testing code
-        serviceStarted();
+        listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1);
+        listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(listAdapter);
 
-        /*
-        Start of first gimbal testing code.
-         */
+        listAdapter.add("Setting Gimbal API Key");
+        listAdapter.notifyDataSetChanged();
+        Gimbal.setApiKey(this.getApplication(), "17ec8de5-fa08-47db-b399-cff8a44c64ac");
+
         placeEventListener = new PlaceEventListener() {
             @Override
             public void onVisitStart(Visit visit) {
-                Toast aTestingMessage = Toast.makeText(getBaseContext(), String.format("Start Visit for %s", visit.getPlace().getName()), Toast.LENGTH_LONG);
-                aTestingMessage.show();
+                listAdapter.add(String.format("Start Visit for %s", visit.getPlace().getName()));
+                listAdapter.notifyDataSetChanged();
             }
 
             @Override
             public void onVisitEnd(Visit visit) {
-                Toast aTestingMessage = Toast.makeText(getBaseContext(), String.format("Start Visit for %s", visit.getPlace().getName()), Toast.LENGTH_LONG);
-                aTestingMessage.show();
+                listAdapter.add(String.format("End Visit for %s", visit.getPlace().getName()));
+                listAdapter.notifyDataSetChanged();
             }
         };
 
@@ -79,159 +50,7 @@ public class MainActivity extends Activity {
         placeManager.addListener(placeEventListener);
         placeManager.startMonitoring();
 
-        //This code checks to see if monitoring is actually on
-        if(PlaceManager.getInstance().isMonitoring() == true)
-        {
-            Toast aTestingMessage = Toast.makeText(getBaseContext(), "Test message to indicate PlaceManager is Monitoring :)", Toast.LENGTH_LONG);
-            aTestingMessage.show();
-        }
-        else
-        {
-            Toast aTestingMessage = Toast.makeText(getBaseContext(), "Test message to indicate PlaceManager IS NOT Monitoring :(", Toast.LENGTH_LONG);
-            aTestingMessage.show();
-        }
-
-        /*
-        communicationListener = new CommunicationListener() {
-            @Override
-            public Collection<Communication> presentNotificationForCommunications(Collection<Communication> communications, Visit visit) {
-                for (Communication comm : communications) {
-                    Log.i("INFO", "Place Communication: " + visit.getPlace().getName() + ", message: " + comm.getTitle());
-                }
-                //allow Gimbal to show the notification for all communications
-                return communications;
-            }
-
-            @Override
-            public Collection<Communication> presentNotificationForCommunications(Collection<Communication> communications, Push push) {
-                for (Communication comm : communications) {
-                    Log.i("INFO", "Received a Push Communication with message: " + comm.getTitle());
-                }
-                //allow Gimbal to show the notification for all communications
-                return communications;
-            }
-
-            @Override
-            public void onNotificationClicked(List<Communication> communications) {
-                Log.i("INFO", "Notification was clicked on");
-            }
-        };
-        CommunicationManager.getInstance().addListener(communicationListener);
-        */
-
         CommunicationManager.getInstance().startReceivingCommunications();
-
-        beaconSightingListener = new BeaconEventListener() {
-            @Override
-            public void onBeaconSighting(BeaconSighting sighting) {
-                Toast aTestingMessage = Toast.makeText(getBaseContext(), "Testing message indicating onBeaconSighting method is working", Toast.LENGTH_LONG);
-                aTestingMessage.show();
-                //Log.i("INFO", sighting.toString());
-            }
-        };
-        beaconManager = new BeaconManager();
-        beaconManager.addListener(beaconSightingListener);
-        beaconManager.startListening();
-
-        /*
-        End of first gimbal testing code
-         */
-        /*
-        Start of gimbal testing code that uses the gimbalHelper class.
-         */
-        //gimbalHelper theGimbalHelper = new gimbalHelper();
-        //theGimbalHelper.addBeacon("beacon 1", "You have detected the beacon : beacon 1");
-        //theGimbalHelper.addBeacon("beacon 2", "You have detected the beacon : beacon 2");
-        //theGimbalHelper.addPlace("You have entered the place \"beacon 1\"!", "You have left the place \"beacon 1\"!", "beacon 1");
-        /*
-            TESTING finished for Gimbal code.
-         */
-
-
-        final Button button1 = (Button) findViewById(R.id.button1);
-        button1.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, WhereAmI.class);
-                startActivity(intent);
-                //Once pressed on the button2 it triggers ViewHistoryActivity
-            }
-        });
-        final Button button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ViewMap.class);
-                startActivity(intent);
-                //Once pressed on the button2 it triggers ViewHistoryActivity
-            }
-        });
-        final Button button3 = (Button) findViewById(R.id.button3);
-        button3.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SetupGimbals.class);
-                startActivity(intent);
-                //Once pressed on the button2 it triggers ViewHistoryActivity
-            }
-        });
-        final Button button4 = (Button) findViewById(R.id.button4);
-        button4.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SettingAndHelp.class);
-                startActivity(intent);
-                //Once pressed on the button2 it triggers ViewHistoryActivity
-            }
-        });
-
-        Log.v(TAG, "222222222222222$$$$$$$$$$$$$$$");
-        Log.v(TAG, "22 onCreate called: creating database.");
-        Log.v(TAG, "222222222222222$$$$$$$$$$$$$$$");
-        databaseTest dbTest = new databaseTest(this);
-        dbTest.startTest();
-
-
-
-
-
     }
 
-    void serviceStarted()
-    {
-        Toast aTestingMessage = Toast.makeText(getBaseContext(), "Testing message stating that the proximity service has started.", Toast.LENGTH_LONG);
-        aTestingMessage.show();
-        // this will be invoked if the service has successfully started
-        // bluetooth scanning will be started at this point
-    }
-
-    void serviceFailed(int errorCode, String message)
-    {
-        // this will be called if the service has failed to start
-        Toast aTestingMessage = Toast.makeText(getBaseContext(), "Testing message stating that the proximity service has failed.", Toast.LENGTH_LONG);
-        aTestingMessage.show();
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
